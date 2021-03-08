8b0f0e831909d442073282947e01bb93a9e7a973
francisco <francisco.fernandez89@gmail.com>
2021-03-08 17:47:37 -0300

    Comiit 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  
+  NavLink
+} from "react-router-dom";
+import Inicio from './components/Inicio';
+import Contacto from './components/Contacto';
+import Nosotros from './components/Nosotros';
+import Users from './components/Users';
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className= 'container'>
+      <div className= "btn-group mt-5">
+      <NavLink to="/" className="btn btn-dark" activeClassName="active">
+          Inicio
+        </NavLink>
+        <NavLink to="/nosotros" className="btn btn-dark" activeClassName="active">
+          Nosotros
+        </NavLink>
+        <NavLink to="/contacto" className="btn btn-dark" activeClassName="active">
+          Contacto
+        </NavLink>
+      </div>
+      <hr/>
+      <Switch>
+        
+        <Route path='/nosotros/:id'>
+          <Users />
+          </Route >
+          
+        <Route path="/" exact>
+          <Inicio/>
+        </Route>
+        <Route path="/nosotros" exact>
+          <Nosotros/>
+        </Route>
+        <Route path="/contacto" exact>
+          <Contacto/>
+        </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
